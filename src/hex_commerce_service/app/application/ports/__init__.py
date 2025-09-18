11a6ebf8014a96bc8f281ce1251a0c48e75089e4
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-09-18 20:48:45 +0900

    feat: add initial implementation of application ports and use cases for inventory and order management

---
+++ src/hex_commerce_service/app/application/ports/__init__.py
@@ -0,0 +1,15 @@
+from .events import EventPublisher
+from .ids import IdGenerator
+from .repositories import InventoryRepository, OrderRepository, ProductRepository
+from .time import Clock
+from .unit_of_work import UnitOfWork
+
+__all__ = (
+    "Clock",
+    "EventPublisher",
+    "IdGenerator",
+    "InventoryRepository",
+    "OrderRepository",
+    "ProductRepository",
+    "UnitOfWork",
+)
---
+++ src/hex_commerce_service/app/application/use_cases/__init__.py
@@ -0,0 +1,11 @@
+from .allocate_stock import AllocateStockCommand, AllocateStockResult, AllocateStockUseCase
+from .place_order import PlaceOrderCommand, PlaceOrderResult, PlaceOrderUseCase
+
+__all__ = [
+    "AllocateStockCommand",
+    "AllocateStockResult",
+    "AllocateStockUseCase",
+    "PlaceOrderCommand",
+    "PlaceOrderResult",
+    "PlaceOrderUseCase",
+]
